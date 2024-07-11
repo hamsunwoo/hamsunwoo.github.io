4e0012593467ba67a9402c80c683c38b1c74c95d
sunwoo <summerham22@gmail.com>
2024-07-11 11:54:11 +0900

    SUDDEN에도 시간포맷 추가

--- sudden.sh
+++ sudden.sh
@@ -4,12 +4,13 @@ FILE=~/tmp/_SUDDEN
 
 while true
 do
+DATE=$(date +"%Y%m%d %H:%M:%S")
   if [ -f "$FILE" ]; then
-      echo "SUDDEN!!!"
+      echo "$DATE,SUDDEN!!!"
   else
       clear
       figlet KIA
-      DATE=$(date +"%Y%m%d %H:%M:%S")
+      #DATE=$(date +"%Y%m%d %H:%M:%S")
       echo $DATE
   fi
   sleep 3
