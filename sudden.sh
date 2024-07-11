2f9a462c501d112298e306e1c6dfb727a52f2ac7
sunwoo <summerham22@gmail.com>
2024-07-11 11:28:10 +0900

    시간포맷 수정

--- sudden.sh
+++ sudden.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FILE=~/tmp/_SUDDEN
-DATE=date +"%Y%m%d %H:%M:%S"
+DATE=$(date +"%Y%m%d %H:%M:%S")
 
 while true
 do
@@ -10,7 +10,7 @@ do
   else
       clear
       figlet KIA
-      "$DATE"
+      echo $DATE
   fi
   sleep 3
 done
